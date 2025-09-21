ce6513a751b7138850d04fb16c31507c84f62f69
Yevhen Oshkukov <sy.yevgen.lc@gmail.com>
2025-09-21 14:04:28 +0200

    fix type query from json schema

--- src/modules/feedParser/controller/feed.controller.ts
+++ src/modules/feedParser/controller/feed.controller.ts
@@ -1,10 +1,36 @@
+// import type { FastifyReply, FastifyRequest } from "fastify";
+// import { getFeed } from "../services/feed.service";
+//
+// type FeedQuery = {
+// 	url?: string;
+// 	force?: "1";
+// };
+//
+// export async function getFeedDataController(
+// 	request: FastifyRequest<{ Querystring: FeedQuery }>,
+// 	reply: FastifyReply,
+// ) {
+// 	const { prisma } = request.server;
+// 	const { url, force } = request.query;
+//
+// 	const forceBool = force === "1";
+//
+// 	try {
+// 		const data = await getFeed(prisma, url, forceBool);
+// 		return reply.send(data);
+// 	} catch (e: unknown) {
+// 		request.log.error(e, "feed parsing error");
+// 		return reply.badGateway("Feed fetch/parse failed");
+// 	}
+// }
+
 import type { FastifyReply, FastifyRequest } from "fastify";
+import type { FromSchema } from "json-schema-to-ts";
+import type { schema as getFeedDataSchema } from "../schemas/getFeedData.schema"; // schema import
 import { getFeed } from "../services/feed.service";
 
-type FeedQuery = {
-	url?: string;
-	force?: "1";
-};
+//type is automatically generated from the schema
+type FeedQuery = FromSchema<typeof getFeedDataSchema.querystring>;
 
 export async function getFeedDataController(
 	request: FastifyRequest<{ Querystring: FeedQuery }>,
--- src/modules/feedParser/schemas/getFeedData.schema.ts
+++ src/modules/feedParser/schemas/getFeedData.schema.ts
@@ -1,8 +1,50 @@
-// // modules/feed/schemas/getFeedData.schema.ts
+// // // modules/feed/schemas/getFeedData.schema.ts
+//
+// export const schema = {
+// 	tags: ["feed"],
+// 	summary: "Get feed data",
+// 	response: {
+// 		200: {
+// 			type: "object",
+// 			required: ["sourceUrl", "items"],
+// 			properties: {
+// 				sourceUrl: { type: "string" },
+// 				items: {
+// 					type: "array",
+// 					items: {
+// 						type: "object",
+// 						properties: {
+// 							title: { type: ["string", "null"] },
+// 							link: { type: ["string", "null"] },
+// 							isoDate: { type: ["string", "null"] },
+// 							pubDate: { type: ["string", "null"] },
+// 							description: { type: ["string", "null"] },
+// 						},
+// 						additionalProperties: false,
+// 					},
+// 				},
+// 			},
+// 			additionalProperties: false,
+// 		},
+// 	},
+// } as const;
+
+// src/modules/feed/schemas/getFeedData.schema.ts
 
 export const schema = {
 	tags: ["feed"],
 	summary: "Get feed data",
+
+	// query parameter typing
+	querystring: {
+		type: "object",
+		properties: {
+			url: { type: "string", format: "uri" }, // opt
+			force: { type: "string", enum: ["1"] }, // only '1', opt
+		},
+		additionalProperties: false,
+	} as const,
+
 	response: {
 		200: {
 			type: "object",

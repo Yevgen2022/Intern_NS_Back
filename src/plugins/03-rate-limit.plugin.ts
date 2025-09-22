b054319337a805d9b6fed73024c14ad8f36042d7
Yevhen Oshkukov <sy.yevgen.lc@gmail.com>
2025-09-22 19:48:08 +0200

    Install plugins and register

---
+++ src/plugins/03-rate-limit.plugin.ts
@@ -0,0 +1,15 @@
+import rateLimit from '@fastify/rate-limit';
+import fp from 'fastify-plugin';
+
+const pluginName = 'rate-limit-plugin';
+
+export default fp(async (fastify) => {
+    // register, but disable limits
+    await fastify.register(rateLimit, {
+        global: false, // do not apply to all routes
+        max: Number.MAX_SAFE_INTEGER, // conditionally “without limit”
+        timeWindow: '1 hour',
+    });
+
+    fastify.pluginLoaded?.(pluginName);
+}, { name: pluginName });

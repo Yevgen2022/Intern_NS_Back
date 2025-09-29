f84aa8c1ee158f8c047ed05ca39c121f41d3f7f7
Yevhen Oshkukov <sy.yevgen.lc@gmail.com>
2025-09-29 11:11:15 +0200

    fix: add httpErrors to fastify types

--- src/types/fastify.d.ts
+++ src/types/fastify.d.ts
@@ -4,5 +4,14 @@ declare module "fastify" {
 	interface FastifyInstance {
 		config: Config;
 		pluginLoaded: (pluginName: string) => void;
+
+        httpErrors: {
+            badRequest: (message?: string) => Error
+            unauthorized: (message?: string) => Error
+            forbidden: (message?: string) => Error
+            notFound: (message?: string) => Error
+            internalServerError: (message?: string) => Error
+            [key: string]: any
+        }
 	}
 }

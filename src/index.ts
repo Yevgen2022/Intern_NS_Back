754bb4c208191c298af27e045efb93d30733c000
Yevhen Oshkukov <sy.yevgen.lc@gmail.com>
2025-09-29 09:59:14 +0200

    Change intex.ts

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,11 @@ async function start() {
 	// const port = fastify.config.PORT;
 	// const host = fastify.config.HOST;
 
-	const port = Number(process.env.PORT ?? fastify.config.PORT ?? 3500);
-	const host = process.env.HOST ?? fastify.config.HOST ?? "0.0.0.0";
+	// const port = Number(process.env.PORT ?? fastify.config.PORT ?? 3500);
+	// const host = process.env.HOST ?? fastify.config.HOST ?? "0.0.0.0";
+
+    const port = Number(process.env.PORT ?? 3500);
+    const host = process.env.HOST ?? "0.0.0.0";
 
 	////////////////////////////////////
 

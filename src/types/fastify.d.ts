09b19fc3103c5fd2b4119436d36141b2c136950d
Yevhen Oshkukov <sy.yevgen.lc@gmail.com>
2025-09-29 19:55:29 +0200

    fix: enable ts-node files option

--- src/types/fastify.d.ts
+++ src/types/fastify.d.ts
@@ -4,5 +4,13 @@ declare module "fastify" {
 	interface FastifyInstance {
 		config: Config;
 		pluginLoaded: (pluginName: string) => void;
+        httpErrors: {
+            badRequest: (message?: string) => Error;
+            unauthorized: (message?: string) => Error;
+            forbidden: (message?: string) => Error;
+            notFound: (message?: string) => Error;
+            internalServerError: (message?: string) => Error;
+            [key: string]: any;
+        }
 	}
 }

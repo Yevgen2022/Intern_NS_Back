7c8768637fbbe09d1d664d378300d45d5d791876
Yevhen Oshkukov <sy.yevgen.lc@gmail.com>
2025-10-07 17:24:14 +0200

    deploy test

--- src/config/clickhouse.ts
+++ src/config/clickhouse.ts
@@ -2,12 +2,25 @@ import "dotenv/config";
 import { createClient } from "@clickhouse/client";
 
 const clickhouseClient = createClient({
-    host: 'https://at7pcd081l.eu-west-2.aws.clickhouse.cloud:8443',
-    username: 'default',
-    password: 'X0ucLjh_EkyHq',
-    database: 'default',
+    url: process.env.CLICKHOUSE_URL || "",
+    username: process.env.CLICKHOUSE_USER || "default",
+    password: process.env.CLICKHOUSE_PASSWORD || "",
+    database: process.env.CLICKHOUSE_DATABASE || "default",
 });
 
 export default clickhouseClient;
 
 
+
+
+
+    // host: process.env.CLICKHOUSE_URL || "",
+    // username: process.env.CLICKHOUSE_USER || "default",
+    // password: process.env.CLICKHOUSE_PASSWORD || "",
+    // database: process.env.CLICKHOUSE_DATABASE || "default",
+
+
+// host: 'https://at7pcd081l.eu-west-2.aws.clickhouse.cloud:8443',
+//     username: 'default',
+//     password: 'X0ucLjh_EkyHq',
+//     database: 'default',

4e2ec11a170a4439173920bf94f39feb46ab808c
Yevhen Oshkukov <sy.yevgen.lc@gmail.com>
2025-10-08 12:54:46 +0200

    write cors plugin

--- src/plugins/00-cors.plugin.ts
+++ src/plugins/00-cors.plugin.ts
@@ -1,17 +1,23 @@
 import cors from "@fastify/cors";
-import type { FastifyInstance } from "fastify";
+import type {FastifyInstance} from "fastify";
 import fp from "fastify-plugin";
+
 
 // fastify-plugin provides correct typing and allows autoload to work without problems
 export default fp(async function corsPlugin(fastify: FastifyInstance) {
-	await fastify.register(cors, {
-        origin: "*",
-		methods: ["GET", "POST", "OPTIONS"],
-		allowedHeaders: ["*"],
-		credentials: true, // cookie-sessions
-	});
+    await fastify.register(cors, {
+        origin: [
+            "http://localhost:5173", // front in dev
+            "https://adtelligent-internship.vercel.app", // front on Vercel
+            "https://adtelligent-internship-git-main-yevgen2022s-projects.vercel.app", // preview (з Network-скріну)
+        ],
+        methods: ["GET", "POST", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        // allowedHeaders: ["*"],
+        credentials: true, // cookie-sessions
+    });
 
-	fastify.log.info("CORS plugin registered");
+    fastify.log.info("CORS plugin registered");
 });
 
 
